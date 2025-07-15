75ea165960141819cba9db8240180f5ecaebd280
최동혁 <your@email.com>
2025-07-15 20:34:33 +0900

    3

---
+++ quest3.js
@@ -0,0 +1,27 @@
+
+/* 주석 1
+const firstNumber = 10; // 변수 선언
+const secondNumber = 5;
+const operator = '+';
+
+let result;
+
+switch (operator) {
+    case '+': // operator 변수가 +인 경우
+        result = firstNumber + secondNumber;
+        break;
+    case '-': // operator 변수가 -인 경우
+        result = firstNumber - secondNumber;
+        break;
+    case '*': // operator 변수가 *인 경우
+        result = firstNumber * secondNumber;
+        break;
+    case '/': // operator 변수가 /인 경우
+        result = firstNumber / secondNumber;
+        break;
+    default: // 아무것도 아닌 경우
+        result = '유효하지 않은 연산자입니다.';
+}
+
+console.log(`결과: ${result}`);
+*/
