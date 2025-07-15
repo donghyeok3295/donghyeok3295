878a8606be52a87dd4b050352e35bb60914e1c21
최동혁 <your@email.com>
2025-07-15 20:05:37 +0900

    연습

---
+++ quest1.js
@@ -0,0 +1,32 @@
+import readlineSyncModule from 'readline-sync';
+
+/* 변수 1
+let name = `최동혁`;
+let age = 23;
+let height = 184.4;
+
+console.log(name+`\n`+age+`\n`+height);
+*/
+
+/* 변수 2
+let name = `최동혁`;
+let age = 23;
+let height = 184.4;
+
+console.log(name+age+height);
+*/
+
+// 변수 과제
+let write = `1. 작성`;
+let view = `2. 조회`;
+let update = '3. 수정';
+let Delete = `4. 삭제`;
+let Plus = `5. 추가기능`;
+let exit = `6. 종료`;
+
+console.log(write);
+console.log(view);
+console.log(update);
+console.log(Delete);
+console.log(Plus);
+console.log(exit);
